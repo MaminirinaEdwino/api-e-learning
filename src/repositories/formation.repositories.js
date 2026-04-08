4ce8e0811a0bf81dc31cd3cff8eb8bdb0b3d8d8a
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-04-08 21:08:33 +0300

    implement: formation repo

---
+++ src/repositories/formation.repositories.js
@@ -0,0 +1,63 @@
+const Formation = require('../models/Formation');
+const Forum = require('../models/Forum');
+const Cours = require('../models/Cours');
+
+class FormationRepository {
+    
+    // Équivalent de Insert
+    async insert(data) {
+        // data contient { nom_formation: '...' }
+        return await Formation.create(data);
+    }
+
+    // Équivalent de Delete
+    async delete(id) {
+        return await Formation.destroy({
+            where: { id_formation: id }
+        });
+    }
+
+    // Équivalent de Update
+    async update(id, data) {
+        return await Formation.update({
+            nom_formation: data.nom_formation
+        }, {
+            where: { id_formation: id }
+        });
+    }
+
+    // Équivalent de GetAll
+    async getAll() {
+        return await Formation.findAll();
+    }
+
+    // Équivalent de GetAllByNom
+    async getAllByNom() {
+        return await Formation.findAll({
+            order: [['nom_formation', 'ASC']]
+        });
+    }
+
+    // Équivalent de GetById
+    async getById(id) {
+        return await Formation.findByPk(id);
+    }
+
+    // Équivalent de GetForGestionForum
+    // Note : Cette méthode SQL originale semble appartenir à un ForumRepository, 
+    // mais je la convertis ici pour respecter votre structure.
+    async getForGestionForum() {
+        return await Forum.findAll({
+            include: [{
+                model: Cours,
+                attributes: ['titre'],
+                required: true // INNER JOIN
+            }],
+            order: [['date_creation', 'DESC']],
+            raw: true,
+            nest: true
+        });
+    }
+}
+
+module.exports = new FormationRepository();
