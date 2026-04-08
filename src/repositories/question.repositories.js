215657c1644aede7b22c309ebf86c81c1dfc53f9
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-04-08 21:11:56 +0300

    implement: question repo

---
+++ src/repositories/question.repositories.js
@@ -0,0 +1,79 @@
+const Question = require('../models/Question');
+
+class QuestionRepository {
+
+    /**
+     * Équivalent de Insert
+     */
+    async insert(data) {
+        return await Question.create({
+            quiz_id: data.quiz_id,
+            texte: data.texte,
+            reponse_correcte: data.reponse_correcte,
+            reponse_incorrecte_1: data.reponse_incorrecte_1,
+            reponse_incorrecte_2: data.reponse_incorrecte_2,
+            reponse_incorrecte_3: data.reponse_incorrecte_3
+        });
+    }
+
+    /**
+     * Équivalent de GetAll
+     */
+    async getAll() {
+        return await Question.findAll();
+    }
+
+    /**
+     * Équivalent de GetById
+     */
+    async getById(id) {
+        return await Question.findByPk(id);
+    }
+
+    /**
+     * Équivalent de Update
+     */
+    async update(id, data) {
+        return await Question.update({
+            quiz_id: data.quiz_id,
+            texte: data.texte,
+            reponse_correcte: data.reponse_correcte,
+            reponse_incorrecte_1: data.reponse_incorrecte_1,
+            reponse_incorrecte_2: data.reponse_incorrecte_2,
+            reponse_incorrecte_3: data.reponse_incorrecte_3
+        }, {
+            where: { id: id }
+        });
+    }
+
+    /**
+     * Équivalent de Delete
+     */
+    async delete(id) {
+        return await Question.destroy({
+            where: { id: id }
+        });
+    }
+
+    /**
+     * Équivalent de GetByQuizId
+     * Récupère toutes les questions d'un quiz spécifique
+     */
+    async getByQuizId(quizId) {
+        return await Question.findAll({
+            where: { quiz_id: quizId }
+        });
+    }
+
+    /**
+     * Équivalent de DeleteByQuiId
+     * Supprime toutes les questions associées à un quiz
+     */
+    async deleteByQuizId(quizId) {
+        return await Question.destroy({
+            where: { quiz_id: quizId }
+        });
+    }
+}
+
+module.exports = new QuestionRepository();
