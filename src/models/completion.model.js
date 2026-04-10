c74de29a71aa912744b4d6213cd2e3696fe7691e
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-04-10 20:56:22 +0300

    fix: completion model

--- src/models/completion.model.js
+++ src/models/completion.model.js
@@ -1,12 +1,54 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../config/database'); // Ton instance de connexion
 
-const Completion = sequelize.define('Completion', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    utilisateur_id: { type: DataTypes.INTEGER, allowNull: false },
-    module_id: { type: DataTypes.INTEGER, allowNull: false },
-    cours_id: { type: DataTypes.INTEGER, allowNull: false },
-    date_completion: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
-}, { tableName: 'completions', timestamps: false });
+class Completion extends Model { }
+
+Completion.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    utilisateur_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'utilisateurs',
+            key: 'id'
+        }
+    },
+    module_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'modules',
+            key: 'id'
+        }
+    },
+    cours_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'cours',
+            key: 'id'
+        }
+    },
+    date_completion: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    }
+}, {
+    sequelize,
+    modelName: 'Completion',
+    tableName: 'completions',
+    timestamps: false, // On utilise date_completion à la place de updatedAt/createdAt
+    underscored: true,
+    indexes: [
+        {
+            unique: true,
+            fields: ['utilisateur_id', 'module_id']
+        }
+    ]
+});
 
 module.exports = Completion;
