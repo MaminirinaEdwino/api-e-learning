44e230dd3758c275e8f19466077a04cdaa9929ed
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-04-11 10:09:19 +0300

    add: token verification in cours routes

--- src/routes/coursRoutes.js
+++ src/routes/coursRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const coursCtrl = require('../controllers/coursController');
+const verifyToken = require('../middlewares/authMiddleware')
 
 // Configuration Multer pour la mémoire (plus simple pour traiter les fichiers ensuite)
 const storage = multer.memoryStorage();
@@ -9,20 +10,22 @@ const upload = multer({ storage: storage });
 
 // Middleware de sécurité
 const isFormateur = (req, res, next) => {
-    if (req.session.user_role === 'formateur') return next();
-    res.redirect('/admin/login');
+    if (req.user.role === 'formateur' && req.user) return next();
+    res.json({
+        message: "Unauthorized"
+    });
 };
 
 // Routes
-router.get('/cours/new', isFormateur, async (req, res) => {
+router.get('/cours/new', verifyToken,isFormateur, async (req, res) => {
     const formations = await formationRepo.getAllByNom();
     res.render('cours/create', { formations });
 });
 
 // "any()" est utilisé ici car les noms des fichiers sont dynamiques (tableaux de modules)
-router.post('/cours/new', isFormateur, upload.any(), coursCtrl.createCours);
+router.post('/cours/new', verifyToken,isFormateur, upload.any(), coursCtrl.createCours);
 
-router.get('/cours/formateur', isFormateur, coursCtrl.listForFormateur);
-router.get('/cours/delete/:id', isFormateur, coursCtrl.deleteCours);
+router.get('/cours/formateur',verifyToken, isFormateur, coursCtrl.listForFormateur);
+router.get('/cours/delete/:id',verifyToken,  isFormateur, coursCtrl.deleteCours);
 
 module.exports = router;
